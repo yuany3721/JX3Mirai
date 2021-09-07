410604713bedeb486990c4294fc4b96ef47a63fb
yuany3721 <li1116@mail.ustc.edu.cn>
2021-09-07 20:07:42 +0800

    新增好友消息处理器

--- src/main/java/top/yuany3721/JX3Mirai/handler/FriendMessageHandler.java
+++ src/main/java/top/yuany3721/JX3Mirai/handler/FriendMessageHandler.java
@@ -3,17 +3,26 @@ package top.yuany3721.JX3Mirai.handler;
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
 import net.mamoe.mirai.event.events.MessageEvent;
-import net.mamoe.mirai.message.data.Face;
+import net.mamoe.mirai.message.data.Image;
 import net.mamoe.mirai.message.data.PlainText;
+import net.mamoe.mirai.utils.ExternalResource;
 import org.jetbrains.annotations.NotNull;
-import top.yuany3721.JX3Mirai.util.BaseProperties;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
- * Stranger Message Handler
+ * Friend Message Handler
  */
-public class StrangerMessageHandler extends SimpleListenerHost {
+public class FriendMessageHandler extends SimpleListenerHost {
     @EventHandler
-    public void onMessage(@NotNull MessageEvent event) {
-        event.getSubject().sendMessage(new PlainText(BaseProperties.botAlias + "不跟陌生人说话！").plus(new Face(307)));
+    public void onMessage(@NotNull MessageEvent event) throws IOException {
+        if (event.getSubject().getId() == 2019358408L) {
+            event.getSubject().sendMessage(new PlainText("test"));
+            File file = new File("G:\\pixiv\\80-100\\304君_41973625_微博头像_00.jpg");
+            Image img = ExternalResource.uploadAsImage(new FileInputStream(file), event.getSubject());
+            event.getSubject().sendMessage(img);
+        }
     }
 }
